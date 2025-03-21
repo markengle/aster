a107d7bbeb174364bd36b9dfecf7098109a207b5
markengle <mark@markengle.de>
2025-03-21 17:38:25 +0100

    added player class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import CircleShape
 
 def main():
     pygame.init()
@@ -12,14 +13,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     continue_game_loop = True
     while continue_game_loop:
+        #dt = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
         pygame.display.flip()
+        local_delta = game_clock.tick(60)
+        dt = (local_delta / 1000)
+
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,20 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.rotation = 0
+
+        super().__init__(self.x, self.y, PLAYER_RADIUS)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
