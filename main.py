7b37c510702efacf3c25199849b874805d626690
markengle <mark@markengle.de>
2025-03-21 11:19:56 +0100

    added pygame and ability to quit the loop by closing the window

--- main.py
+++ main.py
@@ -2,4 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    continue_game_loop = True
+    while continue_game_loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
